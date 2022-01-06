12a950bfff66f6b4d497f4b3d5c50fcc0af1e9ae
Dragomir Jovanovic <dj20180232@student.fon.bg.ac.rs>
2022-01-06 18:12:00 +0100

    Inicijalno kreiranje npm projekta

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './assets/css/bootstrap.min.css';
+import './assets/css/main.css';
+
+function App() {
+    return (
+        <React.StrictMode>
+            
+        </React.StrictMode>
+    )
+}
+
+export default App;
